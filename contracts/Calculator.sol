17a70b6c8f57567f0a3b1f673567757973225d55
NaVindu0315 <navindulakshan99@gmail.com>
2024-01-29 23:17:22 +0530

    multiply function

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -6,12 +6,16 @@ pragma solidity ^0.8.22;
 contract Calculator{
     uint256 result = 0;
 
-    function add(uint num) public {
+    function add(uint256 num) public {
         result +=num;
     }
 
-    function substract(uint num) public {
+    function substract(uint256 num) public {
         result -= num;
+    }
+
+    function multiply(uint256 num) public {
+        result *= num;
     }
 
     
