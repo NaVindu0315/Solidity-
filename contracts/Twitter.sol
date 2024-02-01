d379ca074b8d814eee0023fc7f5666d5801fb80c
NaVindu0315 <navindulakshan99@gmail.com>
2024-02-01 20:08:44 +0530

    adding character limit for tweets

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.22;
 
 contract Twitter {
 
+    uint16 constant MAX_TWEET_Length = 280;
+
     ///struct for the tweets
     struct Tweet{
         address author;
@@ -23,7 +25,7 @@ contract Twitter {
         function createTweet(string memory _tweet) public {
                 ///  condintional 
                 /// legth <=280
-                require(bytes(_tweet).length<=280)
+                require(bytes(_tweet).length<=280,"Tweet is too Long");
 
             Tweet memory newTweet = Tweet({
                 author : msg.sender,
