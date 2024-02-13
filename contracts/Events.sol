49b787370b23460c619ecd1e0c47ecb36e6f1dfd
NaVindu0315 <navindulakshan99@gmail.com>
2024-02-13 22:36:03 +0530

    new events

--- contracts/Events.sol
+++ contracts/Events.sol
@@ -1,3 +1,14 @@
 //SPDX-License-Identifier: MIT
 //tp dp :
 pragma solidity ^0.8.22;
+
+contract EventExample
+{
+    event NewUserRegistered();
+
+    struct User
+    {
+        string username;
+        uint256 age;
+    }
+}
