9b8c74201493057fa84fc2e8057cd14623c71d05
NaVindu0315 <navindulakshan99@gmail.com>
2024-02-29 10:17:20 +0530

    new

---
+++ contracts/test1.sol
@@ -0,0 +1,13 @@
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.22;
+
+// Creating a contract
+contract navindu
+{
+	// Defining a function
+	function get_output() public pure returns (string memory){
+		return ("Hi, your contract ran successfully");
+	}
+}
